9a8c7bf22b173fe082068a4ed7455b2c76df200a
Lisa <ljwedin@gmail.com>
2021-09-12 20:59:35 +0200

    Adds local storage functionality

--- src/components/ToDoCalendar.jsx
+++ src/components/ToDoCalendar.jsx
@@ -2,47 +2,34 @@ import { useState, useEffect } from 'react';
 import MonthView from './MonthView';
 
 const ToDoCalendar = () => {
-    const [toDoList, setToDoList] = useState([
-        {
-            id: 0,
-            date: new Date(2021, 8, 21),
-            toDo: 'irritera erik',
-            done: false
-        },
-        {
-            id: 1,
-            date: new Date(2021, 8, 22),
-            toDo: '???',
-            done: false
-        },
-        {
-            id: 2,
-            date: new Date(2021, 8, 22),
-            toDo: 'profit',
-            done: false
-        },
-        {
-            id: 3,
-            date: new Date(2021, 7, 20),
-            toDo: 'steal underpants',
-            done: false
-        }
-    ]);
+    const [toDoList, setToDoList] = useState([], () => {
+        getLocalStorage();
+    });
 
     const [date, setDate] = useState(new Date());
 
-    // const getLocalStorage = () => {
-    //     const storedToDoList = JSON.parse(localStorage.getItem('toDoList'));
-    //     setToDoList(storedToDoList);
-    // }
+    const getLocalStorage = () => {
+        let storedToDoList = JSON.parse(localStorage.getItem('toDoList'));
 
-    // useEffect(() => {
-    //     getLocalStorage();
-    // }, []);
+        storedToDoList = storedToDoList.map((item) => {
+            item.date = new Date(item.date);
+            return item;
+        })
+
+        setToDoList(storedToDoList);
+    }
+
+    const setLocalStorage = () => {
+        localStorage.setItem('toDoList', JSON.stringify(toDoList))
+    }
 
     useEffect(() => {
-        console.log(date)
-    });
+        getLocalStorage();
+    }, [])
+
+    useEffect(() => {
+        setLocalStorage();
+    }, [toDoList]);
 
     return (
         <div id="content">
--- src/components/ToDoDisplay.jsx
+++ src/components/ToDoDisplay.jsx
@@ -1,12 +1,19 @@
 import ToDoItems from './ToDoItems';
 
 const ToDoDisplay = (props) => {
+    const emptyLocalStorage = () => {
+        localStorage.setItem('toDoList', JSON.stringify([]));
+        props.setToDoList([]);
+        console.log('Töm!');
+    }
+
     return (
         <div>
             <h2>Att-göra-lista:</h2>
             <div id="toDoDisplay">
                 <ToDoItems toDoList={props.toDoList} setToDoList={props.setToDoList} />
             </div>
+            <button onClick={emptyLocalStorage} className="empty">Töm hela listan</button>
         </div>
     )
 }
